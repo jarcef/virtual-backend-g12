ffd9585c30d400a4c557f7aa042eff7bc5219168
jarcef <juliarcef@gmail.com>
2022-04-04 19:48:36 -0500

    limpieza

--- dtos/change_password_dto.py
+++
@@ -1,7 +0,0 @@
-from config import validador
-from marshmallow import fields, validate
-
-class ChangePasswordRequestDTO(validador.Schema):
-    token = fields.String(required=True)
-    #Falta actualizar la información
-    password = fields.String()
