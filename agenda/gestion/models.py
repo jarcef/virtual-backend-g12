7cfaa1a957a5d5360aa906bd1f1a00f745b228f0
jarcef <juliarcef@gmail.com>
2022-04-04 22:46:59 -0500

    actualizacion del inicio de django

--- agenda/gestion/models.py
+++ agenda/gestion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Etiqueta(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, null=False)
+    nombre = models.CharField(max_length=20, unique=True, null=False)
+
+    createAt = models.DateTimeField(auto_now_add=True, db_column='created_at')
+    updateAt = models.DateTimeField(auto_now=True, db_column='updated_at')
+
+    class Meta:
+        db_table = 'etiquetas'
+        ordering = ['-nombre']
